89f3946c321237bca09de794660cfbd4092d95f4
abhishek devop <standup787059@gmail.com>
2025-08-25 10:50:04 +0530

    AI-integrated

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -2,12 +2,12 @@ const { Server } = require("socket.io");
 const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
+const aiService = require("../service/ai.service");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
   io.use(async (socket, next) => {
     const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
-
 
     if (!cookies.token) {
       next(new Error("Authentication error: No token provided"));
@@ -20,14 +20,30 @@ function initSocketServer(httpServer) {
       socket.user = user;
 
       next();
-
     } catch (err) {
       next(new Error("Authentication error: No token provided"));
     }
   });
 
   io.on("connection", (socket) => {
-     
+
+
+    socket.on("ai-message", async (messagePayload) => {
+
+
+      // messagePayload = {
+      //   chat:chat.Id,
+      //   content: message text content
+      // }
+
+      console.log(messagePayload);
+      const response = await aiService.generateResponse(messagePayload.content)
+
+      socket.emit('ai-response', {
+        content: response,
+        chat: messagePayload.chat
+      })
+    });
   });
 }
 
