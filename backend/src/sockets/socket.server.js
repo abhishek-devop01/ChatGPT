f96c2521b76584f79251ad65243fff3b1dcc3729
abhishek devop <standup787059@gmail.com>
2025-08-25 10:15:28 +0530

    implemented socket.io

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,13 +1,34 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
-function initSocketServer(httpServer){
-     const io = new Server(httpServer,{})
+function initSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
-     io.on("connection", (socket) =>{
-          console.log("New socket connection:", socket.id);
-          
-     })
+
+    if (!cookies.token) {
+      next(new Error("Authentication error: No token provided"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+
+      socket.user = user;
+
+      next();
+
+    } catch (err) {
+      next(new Error("Authentication error: No token provided"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+     
+  });
 }
 
-
-module.exports = initSocketServer
+module.exports = initSocketServer;
