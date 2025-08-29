a31267eb4f6a63b4d5d07e74e40095ee96a0c7a8
abhishek devop <standup787059@gmail.com>
2025-08-30 01:28:04 +0530

    landing page

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 const Start = () => {
   return (
     <div>
-      <div>Landing Page</div>
-      <div>
+      <div className="text-9xl text-center font-semibold">Landing Page</div>
+      <div className="text-7xl mt-10 text-center font-semibold">GPT</div>
+      <div className="flex gap-5 items-center justify-center mt-10 text-2xl">
         <Link to="/login">Login</Link>
         <Link to="/login">register</Link>
       </div>
